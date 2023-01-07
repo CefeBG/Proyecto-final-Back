4b4a911c11699b74149e9810c734e2c49a04ac23
Cristian <ceffe.94@gmail.com>
2023-01-07 00:57:21 -0300

    dalequeva

--- index.js
+++ index.js
@@ -29,7 +29,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin:'https://dapper-jalebi-38551b.netlify.app/',
+  credentials:true,
+  optionsSuccessStatus:200
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
